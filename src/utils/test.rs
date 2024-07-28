f64feda35a0d8c80c4310e54535ce55b63ea4c72
Txuritan <txuritan@protonmail.com>
2024-07-28 15:33:26 -0400

    rework test help to support setting accepted, sent, and expected content-types to prepare for content-type negotiation

--- src/utils/test.rs
+++ src/utils/test.rs
@@ -2,14 +2,25 @@ use std::fmt::Debug;
 
 use axum::{
     body::Body,
-    http::{header, Method, Request, StatusCode, Uri},
+    http::{header, HeaderValue, Method, Request, StatusCode, Uri},
     Router,
 };
 use http_body_util::BodyExt as _;
+use mediatype::{
+    names::{APPLICATION, CHARSET, JSON, XML},
+    values::UTF_8,
+    MediaTypeBuf,
+};
 use pretty_assertions::assert_eq;
 use tower::ServiceExt as _;
 
 use crate::database::Database;
+
+#[derive(Clone, Copy)]
+pub enum Format {
+    Json,
+    Xml,
+}
 
 pub struct TestBuilder<T>
 where
@@ -17,9 +28,11 @@ where
 {
     app: Router,
     method: Method,
+    accepts: Format,
     authorization: bool,
     url: Uri,
-    body: Option<Body>,
+    content: Format,
+    body: Option<(Format, Body)>,
     status: StatusCode,
     expected: T,
 }
@@ -36,8 +49,10 @@ where
         Self {
             app,
             method: Method::GET,
+            accepts: Format::Json,
             authorization: false,
             url: url.try_into().unwrap(),
+            content: Format::Json,
             body: None,
             status: StatusCode::OK,
             expected,
@@ -48,6 +63,10 @@ where
         Self { method, ..self }
     }
 
+    pub fn accepts(self, accepts: Format) -> Self {
+        Self { accepts, ..self }
+    }
+
     pub fn authorization(self, authorization: bool) -> Self {
         Self {
             authorization,
@@ -55,9 +74,13 @@ where
         }
     }
 
-    pub fn body(self, body: Body) -> Self {
+    pub fn content(self, content: Format) -> Self {
+        Self { content, ..self }
+    }
+
+    pub fn body(self, format: Format, body: Body) -> Self {
         Self {
-            body: Some(body),
+            body: Some((format, body)),
             ..self
         }
     }
@@ -70,12 +93,24 @@ where
         let Self {
             app,
             method,
+            accepts,
             authorization,
             url,
+            content,
             body,
             status,
             expected,
         } = self;
+
+        let accept_type = match accepts {
+            Format::Json => MediaTypeBuf::new(APPLICATION, JSON),
+            Format::Xml => MediaTypeBuf::new(APPLICATION, XML),
+        };
+
+        let content_type = match content {
+            Format::Json => MediaTypeBuf::new(APPLICATION, JSON),
+            Format::Xml => MediaTypeBuf::from_parts(APPLICATION, XML, None, &[(CHARSET, UTF_8)]),
+        };
 
         let builder = Request::builder().method(method);
 
@@ -85,27 +120,77 @@ where
             builder
         };
 
-        let builder = builder
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .uri(url);
+        let builder = builder.uri(url).header(
+            header::ACCEPT,
+            HeaderValue::from_str(&accept_type.to_string())
+                .expect("Accept header value was not valid, this should not happen"),
+        );
 
-        let request = if let Some(body) = body {
-            builder.body(body)
+        let request = if let Some((format, body)) = body {
+            let format_type = match format {
+                Format::Json => MediaTypeBuf::new(APPLICATION, JSON),
+                Format::Xml => MediaTypeBuf::new(APPLICATION, XML),
+            };
+
+            builder
+                .header(
+                    header::CONTENT_TYPE,
+                    HeaderValue::from_str(&format_type.to_string())
+                        .expect("Content-Type header value was not valid, this should not happen"),
+                )
+                .body(body)
         } else {
             builder.body(Body::empty())
         }
-        .unwrap();
+        .expect("Failed to build request");
 
-        let response = app.oneshot(request).await.unwrap();
+        let response = app.oneshot(request).await.expect("Failed to run request");
 
-        assert_eq!(response.status(), status);
+        assert_eq!(
+            response.status(),
+            status,
+            "Response status code did not match the expected value"
+        );
 
-        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let header = response
+            .headers()
+            .get(header::CONTENT_TYPE)
+            .expect("Missing content-type header");
+        let header = String::from_utf8(header.as_bytes().to_vec())
+            .expect("Header content-type did not return using UTF-8");
+        let header = header
+            .parse::<MediaTypeBuf>()
+            .expect("Header content-type did not respond with a valid media type");
 
-        assert!(!body.is_empty());
+        assert_eq!(
+            content_type, header,
+            "Content-Type header did not match the expected valid"
+        );
 
-        let body: T = serde_json::from_slice(&body[..]).unwrap();
+        let body = response
+            .into_body()
+            .collect()
+            .await
+            .expect("Failed to collect response body")
+            .to_bytes();
 
-        assert_eq!(expected, body);
+        assert!(!body.is_empty(), "Response body should never be empty");
+
+        let body = match content {
+            Format::Json => {
+                serde_json::from_slice(&body[..]).expect("Failed to deserialize response body")
+            }
+            Format::Xml => {
+                let text = String::from_utf8(body.to_vec())
+                    .expect("Failed to convert body into UTF-8 text");
+
+                quick_xml::de::from_str(&text).expect("Failed to deserialize response body")
+            }
+        };
+
+        assert_eq!(
+            expected, body,
+            "Response body did not match the expected value"
+        );
     }
 }
